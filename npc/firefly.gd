c6744301b6415ca548f5a76d0874fe62dc2d738e
Edjay Dev <edjay123123@gmail.com>
2026-08-20 21:34:21 +0800

    Update firefly.gd

--- npc/firefly.gd
+++ npc/firefly.gd
@@ -1,10 +1,12 @@
 extends Node2D
+
 @onready var point_light: PointLight2D = $PointLight2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_start_glow_tween()
 
-func _process(_delta: float) -> void:
-	point_light.energy = randf_range(0.75, 1.5)
+func _start_glow_tween() -> void:
+	var tween = create_tween().set_loops()
+	# Smoothly pulse energy between 0.5 and 1.8 over 1.2 seconds
+	tween.tween_property(point_light, "energy", 1.8, 1.2).from(0.5)
+	tween.tween_property(point_light, "energy", 0.5, 1.2).from(1.8)
